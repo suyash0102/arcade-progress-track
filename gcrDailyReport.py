94f7fa7924e3921a2d3630a5769689f6321004ba
suyash_dahake <suyashdahake410@gmail.com>
2023-07-23 14:05:52 +0530

    update

--- gcrDailyReport.py
+++ gcrDailyReport.py
@@ -172,10 +172,10 @@ if (sidebarContent == "Progress Report"):
             st.markdown('<b class="big-font">Milestone Status</b>',
                         unsafe_allow_html=True)
 
-            quest, skillbg,game, per = milestoneCal(40, 40, tindex)
+            quest, skillbg,game, per = milestoneCal(2,30, 4, tindex)
             st.subheader("You have completed " + str(quest) +
                          " Quests and " + str(skillbg) + " Skill Badges.")
-            if(quest >= 10 and skillbg >= 5):
+            if(quest >= 1 and skillbg >= 9 and game>=2):
                 st.balloons()
 
             # Milestone1
